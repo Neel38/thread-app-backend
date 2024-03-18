713d28479c22703662c353dfa2e18f33dee5ddaa
Neel38 <neeltundiya5454@gmail.com>
2024-03-18 15:19:05 +0530

    Prisma setup Done

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,61 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import { prismaClient } from "./lib/db";
+
 //import cors from 'cors';
 
 async function init() {
   const app = express();
 
+  //middlewares
+  app.use(express.json());
+
   //creating  graphql  server
   const gqpServer = new ApolloServer({
     typeDefs: ` 
-        type Query {
-            hello :String 
-            say(name:String):String
-        }
+    type Query {
+      hello :String 
+      say(name:String):String
+  }
+  type Mutation {
+    createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+  }
 
-    `,   // schema 
+
+    `, // schema
     resolvers: {
-        Query:{
-            hello : ()=>`Hello from graphql`,
-            say :(_,{name} :{name: String})=>`hey ${name} , how are you?`,
-            
-        }
+      Query: {
+        hello: () => `Hello from graphql`,
+        say: (_, { name }: { name: String }) => `hey ${name} , how are you?`,
+      },
+      Mutation: {
+        createUser: async (
+          _,
+          {
+            firstName,
+            lastName,
+            email,
+            password,
+          }: {
+            firstName: string;
+            lastName: string;
+            email: string;
+            password: string;
+          }
+        ) => {
+          await prismaClient.user.create({
+            data: {
+              email,
+              firstName,
+              lastName,
+              password,
+              salt: "randam_salt",
+            },
+          });
+          return true;
+        },
+      },
     },
   });
 
@@ -28,18 +63,14 @@ async function init() {
   await gqpServer.start();
 
   const PORT = Number(process.env.PORT) || 8000;
+
   
-
-  //middlewares
-  app.use(express.json())
-
-
   app.get("/", (req, res) => {
-      return res.json({ message: "server is up and running.." });
-    });
+    return res.json({ message: "server is up and running.." });
+  });
 
-    app.use("/graphql",expressMiddleware(gqpServer));
-    
+  app.use("/graphql", expressMiddleware(gqpServer));
+
   app.listen(PORT, () => console.log(`server is strated at PORT:${PORT}`));
 }
 init();
